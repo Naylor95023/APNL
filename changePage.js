62612bf0c2694d34ef5f1deb97fad49da8f5cf38
Naylor95023 <naylor95023@gmail.com>
2016-03-14 16:05:14 +0800

    JSON+Angular/but not show

--- changePage.js
+++ changePage.js
@@ -5,3 +5,25 @@ app.controller('_tabCtrl', function ($scope) {
         $scope.currentTab = index;
     }
 });
+
+var newsApp =  angular.module('_newApp',[]);
+newsApp.controller('showNewsCtrl',function($scope, $http){
+    $http.get('../news.json').then(function(response){
+            $scope.NEWS = response.data;
+        });
+});
+
+angular.module("_newApp").directive("_contentApp", function() {
+    return {
+        restrict: "E",
+        templateUrl: "news.html"
+    }
+})
+
+
+var contentApp = angular.mudule('_contentApp',[]);
+contentApp.controller('_contentCtrl', function ($scope) {
+    $scope.onClickTab = function (tab) {
+
+    }
+});
