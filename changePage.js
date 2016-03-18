3c3202ed0511614cdbca8b05cff5808880a95d9f
Naylor95023 <naylor95023@gmail.com>
2016-03-18 11:15:53 +0800

    把News放回home

--- changePage.js
+++ changePage.js
@@ -1,29 +1,21 @@
-var app = angular.module('_tabApp',[]);
-app.controller('_tabCtrl', function ($scope) {
-    $scope.onClickTab = function (tab) {
-        var index = tab + ".html";
-        $scope.currentTab = index;
-    }
-});
+(function() {
+    var app = angular.module('_tabApp', []);
+    app.controller('_tabCtrl', function ($scope) {
+        $scope.currentTab = 'homeContent.html';
 
+        $scope.onClickTab = function (tab) {
+            var index = "'" + tab + ".html'";
+            $scope.currentTab = index;
+        }
+    });
+})();
+
+(function() {
 var newsApp =  angular.module('_newApp',[]);
 newsApp.controller('showNewsCtrl',function($scope, $http){
     $http.get('../news.json').then(function(response){
             $scope.NEWS = response.data;
         });
 });
+})();
 
-angular.module("_newApp").directive("_contentApp", function() {
-    return {
-        restrict: "E",
-        templateUrl: "news.html"
-    }
-})
-
-
-var contentApp = angular.mudule('_contentApp',[]);
-contentApp.controller('_contentCtrl', function ($scope) {
-    $scope.onClickTab = function (tab) {
-
-    }
-});
