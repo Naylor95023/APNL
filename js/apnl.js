327bcee1bb0f7be8b18a60393d1469f328cacc43
unknown <you@example.com>
2016-03-26 18:17:41 +0800

    publication

--- js/apnl.js
+++ js/apnl.js
@@ -6,6 +6,7 @@
         $routeProvider.when('/news', {templateUrl: 'pages/home.html',controller: 'newsCtrl'});
         $routeProvider.when('/professor', {templateUrl: 'pages/professor.html'});
         $routeProvider.when('/research', {templateUrl: 'pages/research.html'});
+        $routeProvider.when('/publication', {templateUrl: 'pages/publication.html'});
         $routeProvider.otherwise({redirectTo: '/news'});
     }
 })();
