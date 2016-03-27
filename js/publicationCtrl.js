c29bdc63ae757d8f088339a42b9c5b99e9454e0a
Leo880410 <leo8804102004@yahoo.com.tw>
2016-03-28 00:18:53 +0800

    publication: edit :css/publication.css     .titleList     font-family     font-weight publication.html的content改用ng-repeat : data/publication.json

--- js/publicationCtrl.js
+++ js/publicationCtrl.js
@@ -1,15 +1,35 @@
 (function () {
     angular.module('apnl').controller('PublicationCtrl', PublicationCtrl);
 
-    PublicationCtrl.$inject = [];
+    PublicationCtrl.$inject = [$http];
 
-    function PublicationCtrl(){
+    function PublicationCtrl($http){
         var vm = this;
         vm.selectTab = selectTab;
         vm.selectedTab = 'tab1';
+        selectTab('tab1');
 
         function selectTab(tab){
             vm.selectedTab = tab;
+            $http.get('data/publication.json').then(function (response) {
+                switch (vm.selectedTab){
+                    case "tab1":
+                        vm.PUBLICATION = response.data.JournalPapers;
+                        break;
+                    case "tab2":
+                        vm.PUBLICATION = response.data.BookName;
+                        break;
+                    case "tab3":
+                        vm.PUBLICATION = response.data.Patent;
+                        break;
+                    case "tab4":
+                        vm.PUBLICATION = response.data.ConferencePapers;
+                        break;
+                    default:
+                        vm.PUBLICATION = response.data.JournalPapers;
+                        break;
+                }
+            })
         }
     }
 })();
