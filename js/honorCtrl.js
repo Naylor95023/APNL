27453260326512c5e1234a7084645f6cdaf8635e
Naylor95023 <naylor95023@gmail.com>
2016-04-25 11:46:59 +0800

    honor資料建檔、切換頁功能

--- js/honorCtrl.js
+++ js/honorCtrl.js
@@ -2,24 +2,31 @@
     angular.module('apnl').controller('HonorCtrl', HonorCtrl);
 
     HonorCtrl.$inject = ['$http'];
-
     function HonorCtrl($http) {
+        var tab_links = ["#professor_link", "#student_link"];
         var vm = this;
         vm.selectTab = selectTab;
-        vm._tab = 'professor';
-        selectTab(vm._tab);
+        //initialization
+        selectTab('professor');
 
         function selectTab(tab) {
-            vm._tab = tab;
             $http.get('data/honor.json').then(function (response) {
-                if(vm._tab == 'professor'){
-                    console.log("HonorLoad!!");
+                if(tab == 'professor'){
                     vm.EVENT = response.data.Professor;
                 }
                 else{
                     vm.EVENT = response.data.Student;
                 }
             })
+            setactive(tab);
+        }
+
+        function setactive(tab){
+            var link="#" + tab + "_link";
+            for(var i=0; i < tab_links.length; i++){
+                $(tab_links[i]).removeClass('active');
+            }
+            $(link).addClass('active');
         }
     }
 })();
